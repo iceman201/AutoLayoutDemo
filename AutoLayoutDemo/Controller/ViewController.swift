2fd7cd1e5c403a37bd12e4b19c4f315fa5571732
Liguo Jiao <liguo@jiao.co.nz>
2018-09-20 23:00:20 +1200

    update

--- AutoLayoutDemo/Controller/ViewController.swift
+++ AutoLayoutDemo/Controller/ViewController.swift
@@ -10,40 +10,46 @@ import UIKit
 import KeepLayout
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainContainer: UIView!
+    
     var topBar: UIView?
     var topBarTitle: UILabel?
     var topBarMenu: UIButton?
 
+    var safeAreas: UILayoutGuide {
+        get {
+            return self.view.safeAreaLayoutGuide
+        }
+    }
     override func loadView() {
         super.loadView()
 
         let topBarView = UIView()
         topBarView.backgroundColor = .green
-        self.view.addSubview(topBarView)
+        mainContainer.addSubview(topBarView)
         topBarView.keepTopInset.equal = zeroPadding
-        topBarView.keepVerticalInsets.equal = zeroPadding
+        topBarView.keepHorizontalInsets.equal = zeroPadding
         topBarView.keepHeight.equal = defaultNavBarHeight
         self.topBar = topBarView
 
         let label = UILabel()
         label.text = "hahah"
-        self.view.addSubview(label)
-        label.keepSize.equal = 35.keepValue
+        topBarView.addSubview(label)
+        label.keepHeight.equal = 35.keepValue
         label.keepLeftInset.equal = doublePadding
         label.keepVerticallyCentered()
         self.topBarTitle = label
-
-        
-
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.applyGradient(colours: [UIColor.yellowColor(), UIColor.blueColor(), UIColor.redColor()],
+        let top = UIColor(displayP3Red: 248/255, green: 87/255, blue: 165/255, alpha: 1.0)
+        let bottom = UIColor(displayP3Red: 254/255, green: 88/255, blue: 88/255, alpha: 1.0)
+
+        self.view.applyGradient(colours: [top,bottom],
                                 locations: [0.0, 0.5, 1.0])
     }
-
-
+    
 }
+
 
