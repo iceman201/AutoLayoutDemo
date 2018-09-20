76811f7f84f7eb0ed565a0c72229cf5063630dcf
Liguo Jiao <liguo@jiao.co.nz>
2018-09-21 01:30:34 +1200

    update project file

---
+++ AutoLayoutDemo/Controller/CardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  CardViewController.swift
+//  AutoLayoutDemo
+//
+//  Created by Liguo Jiao on 21/09/18.
+//  Copyright © 2018 Liguo Jiao. All rights reserved.
+//
+
+import UIKit
+
+class CardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
