d06f362c0a58e9d7dec8a82588c040c66cf201cc
Liguo Jiao <liguo@jiao.co.nz>
2018-09-26 00:25:30 +1200

    update view controller

--- AutoLayoutDemo/Controller/ViewController.swift
+++ AutoLayoutDemo/Controller/ViewController.swift
@@ -11,11 +11,13 @@ import UIKit
 class ViewController: BaseViewController, UIScrollViewDelegate {
     
     var cardScrollView: UIScrollView?
+    
+    var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+    
     override func loadView() {
         super.loadView()
         let scroll:UIScrollView = {
             let scroll = UIScrollView()
-            scroll.backgroundColor = .green
             scroll.showsHorizontalScrollIndicator = false
             scroll.isPagingEnabled = true
             self.mainContainer.addSubview(scroll)
@@ -33,18 +35,6 @@ class ViewController: BaseViewController, UIScrollViewDelegate {
         let top = UIColor(displayP3Red: 248/255, green: 87/255, blue: 165/255, alpha: 1.0)
         let bottom = UIColor(displayP3Red: 254/255, green: 88/255, blue: 88/255, alpha: 1.0)
         
-        self.cardScrollView?.layoutIfNeeded()
-        var frame = CGRect(x: 0, y: 0, width: 0, height: 0)
-        
-        for eachCard in 0..<3 {
-            frame.origin.x = self.cardScrollView?.frame.size.width ?? 0 * CGFloat(eachCard)
-            frame.size = (self.cardScrollView?.frame.size)!
-            let card = UIView(frame: frame)
-            card.backgroundColor = [.green,.red,.yellow][eachCard]
-            self.cardScrollView?.addSubview(card)
-        }
-        self.cardScrollView?.contentSize = CGSize(width: (self.cardScrollView?.frame.size.width)! * 3, height: (self.cardScrollView?.frame.size.height)!)
-        
 //        for family: String in UIFont.familyNames
 //        {
 //            print("\(family)")
@@ -58,10 +48,24 @@ class ViewController: BaseViewController, UIScrollViewDelegate {
         
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.setCards()
+    }
+    
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         /* get the current select page */
     }
     
+    func setCards() {
+        for eachCard in 0..<3 {
+            frame.origin.x = UIScreen.main.bounds.width * CGFloat(eachCard)
+            frame.size = (self.cardScrollView?.frame.size)!
+            let card = CardView(frame: frame)
+            self.cardScrollView?.addSubview(card)
+        }
+        self.cardScrollView?.contentSize = CGSize(width: (self.cardScrollView?.frame.size.width)! * 3, height: (self.cardScrollView?.frame.size.height)!)
+    }
 }
 
 
